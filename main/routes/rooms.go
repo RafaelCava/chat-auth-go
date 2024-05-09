00501258b9226e81a4fd5c5a4252c8e2510396a6
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2024-05-08 22:12:20 -0300

    feat: create middleware function to validate token

--- main/routes/rooms.go
+++ main/routes/rooms.go
@@ -1,13 +1,45 @@
 package routes
 
 import (
+	"os"
+	"strings"
+
+	"github.com/RafaelCava/chat-auth-go/infra/cryptography/adapters"
 	"github.com/RafaelCava/chat-auth-go/main/factories"
 	"github.com/gin-gonic/gin"
 )
 
 func addRoomsRoutes(rg *gin.RouterGroup) {
 	users := rg.Group("/rooms")
-	users.POST("", func(ctx *gin.Context) {
+	users.POST("", middlewareValidation, func(ctx *gin.Context) {
 		factories.NewCreateRoomControllerFactory().Handle(ctx)
 	})
+}
+
+func middlewareValidation(ctx *gin.Context) {
+	authorizationHeader := ctx.GetHeader("Authorization")
+	if authorizationHeader == "" {
+		ctx.AbortWithStatusJSON(401, gin.H{"error": "No token provided"})
+		return
+	}
+	splitToken := strings.Split(authorizationHeader, " ")
+	prefix := splitToken[0]
+	if prefix != "Bearer" {
+		ctx.AbortWithStatusJSON(401, gin.H{"error": "Invalid token type"})
+		return
+	}
+	token := splitToken[1]
+	if token == "" || token == "null" {
+		ctx.AbortWithStatusJSON(401, gin.H{"error": "Invalid token credential"})
+		return
+	}
+	secretKey := os.Getenv("SECRET_TOKEN")
+	issuerKey := os.Getenv("ISS_TOKEN")
+	encrypterAdapter := adapters.NewEncrypterAdapter(secretKey, issuerKey)
+	claims, err := encrypterAdapter.Decrypt(token)
+	if err != nil {
+		ctx.AbortWithStatusJSON(401, gin.H{"error": "Invalid token"})
+	}
+	ctx.Set("user_id", claims.UserId)
+	ctx.Next()
 }
